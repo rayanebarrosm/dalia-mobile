a0708fe5082f5f88796bc0207f83ac45bf06eacc
Aluno <Aluno@fatecdiadema.com>
2026-04-01 12:45:02 -0300

    RegisterScreen: Box

--- app/src/main/java/com/example/dalia2/ui/theme/screen/RegisterScreen.kt
+++ app/src/main/java/com/example/dalia2/ui/theme/screen/RegisterScreen.kt
@@ -1,22 +1,32 @@
 package com.example.dalia2.ui.theme.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dalia2.R
+import com.example.dalia2.ui.theme.PinkButton
 
 @Composable
 fun QuizRegisterScreen (
@@ -51,9 +61,91 @@ fun QuizRegisterScreen (
         Spacer(modifier = Modifier.height(24.dp))
 
 
-        Box()
+        Box(
+            modifier = Modifier
+                .aspectRatio(1f) // Quadrado perfeito baseado na largura da coluna
+                .padding(2.dp)   // Pequeno espaço entre os dias
+                .background(
+                    shape = RoundedCornerShape(8.dp) // Ou CircleShape para ficar redondo
+        ) {
 
-        Box()
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_happy),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_great),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_neutral),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_grimace),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_upset),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_sad),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_surprised),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.rg_fallinglove),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .size(154.dp, 147.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+         }
+
+        Box(
+              modifier = Modifier
+                .aspectRatio(1f) // Quadrado perfeito baseado na largura da coluna
+                .padding(2.dp)   // Pequeno espaço entre os dias
+                .background(
+                    shape = RoundedCornerShape(8.dp) // Ou CircleShape para ficar redondo
+        ) {
+
+
+
+        }
 
         Box()
 
