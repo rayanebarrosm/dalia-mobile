5459be8a00d9fa612b34f3f2f8e06f9e8141fecd
Ana Beatriz <ana.alves69@fatec.sp.gov.br>
2026-05-11 20:23:45 -0300

    arrumando home e calendario

--- app/src/main/java/com/example/dalia2/ui/theme/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/example/dalia2/ui/theme/viewmodel/CalendarViewModel.kt
@@ -6,13 +6,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.dalia2.data.local.CicloEntity
 import com.example.dalia2.data.model.CycleData
 import com.example.dalia2.data.repository.DaliaRepository
-import dagger.hilt.android.lifecycle.
-import kotlinx.coroutines.CoroutineScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.time.LocalDate
